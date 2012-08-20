952a2e0ed93bba157df2fa3cf5e1349fe2b07b64
Espen Ekvang <espen.ekvang@bekk.no>
2012-08-20 10:32:24 +0200

    Added console app to illustrate consumation of the simple web api

---
+++ webapi/Oppgaver/Bekk.dontnetintro.WebApi.Blog/WebApiConsole/WebApiConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Bekk.dotnetintro.WebApi.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var blogEntries = new WebApiAdapter(args[0]).Get();
+
+            foreach (var blogEntry in blogEntries)
+            {
+                System.Console.WriteLine(blogEntry.Title);
+            }
+
+            System.Console.ReadLine();
+        }
+    }
+}
