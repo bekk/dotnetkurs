5dcfb5a69a4a3e28b61d5652c1d6a3ec645752ca
Espen Ekvang <espen.ekvang@bekk.no>
2012-09-03 11:08:24 +0200

    Added demo project for LINQToSQL and changes to slidedeck

---
+++ persistingData/Demo/Bekk.dotnetintro.Data.LinqToSql/Bekk.dotnetintro.Data.LinqToSql/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Bekk.dotnetintro.Data.LinqToSql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var dataContext = new BlogDataContext())
+            {
+                BlogEntry firstBlogEntry = (from blogEntry in dataContext.BlogEntries 
+                                                     where blogEntry.Id == 1 
+                                                     select blogEntry).FirstOrDefault();
+                
+                Console.WriteLine("First blogentry");
+                Console.WriteLine(string.Format("Id     : {0}", firstBlogEntry.Id));
+                Console.WriteLine(string.Format("Title  : {0}", firstBlogEntry.Title));
+                Console.WriteLine(string.Format("Content: {0}", firstBlogEntry.Content));
+                Console.ReadLine();
+            }
+        }
+    }
+}
