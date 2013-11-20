d69da304391acbd2ad45abde3b7871919ab2a414
Espen Ekvang <espen.ekvang@bekk.no>
2013-11-20 19:55:54 +0100

    Adjustments to slides for webapi and uploaded MinBlogg

---
+++ webapi/Oppgaver/MinBlogg/MinBlogg.Api/Controllers/BloggposterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace MinBlogg.Api.Controllers
+{
+    public class BloggposterController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new[] { "value1", "value2" };
+        }
+
+        public void Post(Bloggpost bloggpost)
+        {
+        }
+    }
+    
+    public class Bloggpost
+    {
+        public int Id { get; set; }
+        public string Tittel { get; set; }
+    }
+}
